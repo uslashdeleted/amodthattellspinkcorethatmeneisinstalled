363d11f8a15403d4cf460badd19769125bec4dfa
uslashdeleted <59172039+uslashdeleted@users.noreply.github.com>
2021-09-13 17:23:56 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -11,31 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
+#
+#
+#
 LOCAL_PATH := $(call my-dir)
 TARGET_ARCH_ABI := $(APP_ABI)
-
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: modloader - version: 1.2.3
+
+# Creating prebuilt for dependency: pinkcore - version: 1.6.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := modloader
-LOCAL_EXPORT_C_INCLUDES := extern/modloader
-LOCAL_SRC_FILES := extern/libmodloader.so
+LOCAL_MODULE := pinkcore
+LOCAL_EXPORT_C_INCLUDES := extern/pinkcore
+LOCAL_SRC_FILES := extern/libpinkcore.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: beatsaber-hook - version: 2.3.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := beatsaber-hook_2_3_0
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
 LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_3_0.so
-LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: pinkcore - version: 1.6.1
+# Creating prebuilt for dependency: modloader - version: 1.2.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := pinkcore
-LOCAL_EXPORT_C_INCLUDES := extern/pinkcore
-LOCAL_SRC_FILES := extern/libpinkcore.so
+LOCAL_MODULE := modloader
+LOCAL_EXPORT_C_INCLUDES := extern/modloader
+LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.12.7
 include $(CLEAR_VARS)
@@ -43,25 +43,15 @@ LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.14.0
-include $(CLEAR_VARS)
-LOCAL_MODULE := codegen
-LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen.so
-include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MEEnabler
-LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
-LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
-LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
-LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
+LOCAL_MODULE := amodthattellspinkcorethatmeneisinstalled
+LOCAL_SRC_FILES := $(call rwildcard,src/,*.cpp)
 LOCAL_SHARED_LIBRARIES += pinkcore
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
+LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_SHARED_LIBRARIES += custom-types
-LOCAL_SHARED_LIBRARIES += codegen
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"#{id}"' -DVERSION='"0.1.0"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
-LOCAL_CPPFLAGS += -std=c++2a
-LOCAL_C_INCLUDES += ./include ./src
+LOCAL_CFLAGS += -isystem'extern' -isystem'extern/libil2cpp/il2cpp/libil2cpp' -Wall -Wextra -DVERSION='"0.20.0"'
+LOCAL_C_INCLUDES += ./include
 include $(BUILD_SHARED_LIBRARY)
